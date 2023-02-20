3f0309b4e777299943338cc75e4bdbf73bb929fd
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-02-20 15:47:22 +0300

    component SkillItem

--- src/components/Skills/SkillItem/SkillItem.tsx
+++ src/components/Skills/SkillItem/SkillItem.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import s from './SkillItem.module.css'
 
-type SkillItemType = {}
+type SkillItemType = {
+    title: string
+    description: string
+}
 
 export const SkillItem = (props: SkillItemType) => {
     return (
         <div className={s.skillItem}>
-
+            <div className={s.icon}></div>
+            <h3>{props.title}</h3>
+            <span className={s.description}>
+                {props.description}
+            </span>
         </div>
     );
 };
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Skills.module.css'
 import StyleContainer from "../commo/styles/Container.module.css";
+import {SkillItem} from "./SkillItem/SkillItem";
 
 
 type SkillsType = {}
@@ -10,7 +11,11 @@ export const Skills = (props: SkillsType) => {
         <div className={s.skillsBlock}>
             <div className={`${StyleContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
-                <div className={s.skills}></div>
+                <div className={s.skills}>
+                    <SkillItem title={'JavaScript'} description={'Programmer Lorem ipsum dolor sit amet, consecte tur adipisicing elit, sed do eiusmod tempor incididunt ut'}/>
+                    <SkillItem title={'React'} description={'Programmer Lorem ipsum dolor sit amet, consecte tur adipisicing elit, sed do eiusmod tempor incididunt ut'}/>
+                    <SkillItem title={'Redux'} description={'Programmer Lorem ipsum dolor sit amet'}/>
+                </div>
             </div>
         </div>
     );
