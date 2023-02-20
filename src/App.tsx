f6c755904c5ff72d741cf4a3173f5dfc540f27ca
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-02-20 08:00:29 +0300

    main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
+import {Main} from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Main/>
     </div>
   );
 }
 
 export default App;
+
+//https://preview.themeforest.net/item/spirit-portfolioresume-html-template-for-developers-programmers-and-freelancers/full_screen_preview/17094383?_ga=2.243555141.1736568102.1676814821-200242335.1676814821
