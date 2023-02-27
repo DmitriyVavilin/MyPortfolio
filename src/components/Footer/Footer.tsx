0ab64ee907f9ab49adfb378d491bc036fcc0aa0d
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-02-27 16:23:57 +0300

    media version_1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,12 +8,12 @@ export const Footer = () => {
         <div className={s.footerBlock}>
             <div className={`${StyleContainer.container} ${s.footerContainer}`}>
                 <h2>Vavilin Dmitryi</h2>
-                <div className={s.footerSocial}>
-                    <SocialNetwork/>
-                    <SocialNetwork/>
-                    <SocialNetwork/>
-                    <SocialNetwork/>
-                </div>
+                    <div className={s.footerSocial}>
+                        <SocialNetwork/>
+                        <SocialNetwork/>
+                        <SocialNetwork/>
+                        <SocialNetwork/>
+                    </div>
                 <h2 className={s.footerText}>2023 All rights reserved</h2>
             </div>
         </div>
