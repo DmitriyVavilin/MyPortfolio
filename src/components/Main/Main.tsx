33b983207129ae2b29752e0eb979d8f60bae334b
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-04-25 13:05:24 +0300

    Second part of the portfolio

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,13 +7,14 @@ type MainType = {}
 export const Main = (props: MainType) => {
     return (
         <div className={s.main}>
-            <div className={StyleContainer.container}>
-                <div className={s.text}>
-                    <h1>Dmitriy Vavilin</h1>
-                    <span>Front-end developer</span>
-                </div>
-                <div className={s.photo}>
-
+            <div className={`${StyleContainer.container} ${s.mainContainer}`}>
+                <div className={s.headerText}>
+                    <span className={s.text}>Hello There!</span>
+                    <h1 className={s.title}>
+                        <span>I'm Dmitriy</span>
+                        <span>I'm Frontend Developer</span>
+                        <span>I Like Coding</span>
+                    </h1>
                 </div>
             </div>
         </div>
