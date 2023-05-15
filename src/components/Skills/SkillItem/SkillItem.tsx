c65e6a6b080584d051e0af2cc3c755aec6d4a97f
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-05-15 16:29:58 +0300

    add svg icons

--- src/components/Skills/SkillItem/SkillItem.tsx
+++ src/components/Skills/SkillItem/SkillItem.tsx
@@ -10,8 +10,8 @@ type SkillItemType = {
 export const SkillItem = (props: SkillItemType) => {
     return (
         <div className={s.skillItem}>
-            <div className={s.icon}>
-                <HomeSvgSelector id={props.id}/>
+            <div>
+                <div className={s.icon_item}><HomeSvgSelector id={props.id}/></div>
                 <div>
                     <span className={s.title}>{props.title}</span>
                 </div>
