0705548b3b5adc67feaf3f61acad0cf4243d1a10
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-05-15 17:38:19 +0300

    main block

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Main.module.css'
 import StyleContainer from '../commo/styles/Container.module.css'
+import {TypeAnimation} from "react-type-animation";
 
 type MainType = {}
 
@@ -11,9 +12,19 @@ export const Main = (props: MainType) => {
                 <div className={s.headerText}>
                     <span className={s.text}>Hello There!</span>
                     <h1 className={s.title}>
-                        <span>I'm Dmitriy</span>
-                        <span>I'm Frontend Developer</span>
-                        <span>I Like Coding</span>
+                        <TypeAnimation
+                            sequence={[
+                               ` 'I'm Dmitriy`,
+                                1700,
+                                `I'm Frontend Developer`,
+                                2000,
+                                'I Like Coding',
+                                2000,
+                            ]}
+                            repeat={Infinity}
+                            wrapper="span"
+                            cursor={true}
+                        />
                     </h1>
                 </div>
             </div>
