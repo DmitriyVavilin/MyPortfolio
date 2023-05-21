6305f86b0a99d532c7edb5fa85b9e29ac7dea6b3
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-05-21 12:30:35 +0300

    main block

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import {Footer} from "./components/Footer/Footer";
 function App() {
     return (
         <div className="App">
-            <Header/>
             <Main/>
             <Skills/>
             <Works/>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,31 +2,34 @@ import React from 'react';
 import s from './Main.module.css'
 import StyleContainer from '../commo/styles/Container.module.css'
 import {TypeAnimation} from "react-type-animation";
+import {Header} from "../Header/Header";
+import MatrixRain from "./MatrixRain";
 
 type MainType = {}
 
 export const Main = (props: MainType) => {
     return (
         <div className={s.main}>
+            <Header/>
             <div className={`${StyleContainer.container} ${s.mainContainer}`}>
-                <div className={s.headerText}>
-                    <span className={s.text}>Hello There!</span>
-                    <h1 className={s.title}>
-                        <TypeAnimation
-                            sequence={[
-                               ` 'I'm Dmitriy`,
-                                1700,
-                                `I'm Frontend Developer`,
-                                2000,
-                                'I Like Coding',
-                                2000,
-                            ]}
-                            repeat={Infinity}
-                            wrapper="span"
-                            cursor={true}
-                        />
-                    </h1>
-                </div>
+                    <div className={s.headerText}>
+                        <span className={s.text}>Hello There!</span>
+                        <h1 className={s.title}>
+                            <TypeAnimation
+                                sequence={[
+                                    ` 'I'm Dmitriy`,
+                                    1700,
+                                    `I'm Frontend Developer`,
+                                    2000,
+                                    'I Like Coding',
+                                    2000,
+                                ]}
+                                repeat={Infinity}
+                                wrapper="span"
+                                cursor={true}
+                            />
+                        </h1>
+                    </div>
             </div>
         </div>
     );
